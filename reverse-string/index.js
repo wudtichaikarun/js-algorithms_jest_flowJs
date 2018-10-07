317f17e721e1fa1b20285257c3de56647b1f321f
Romantic <595159090004@dpu.ac.th>
2018-10-07 13:06:11 +0700

    reverse-string solution#2-#3

--- reverse-string/index.js
+++ reverse-string/index.js
@@ -7,6 +7,7 @@
  * reverse('hello') -> 'olleh'
  */
 
+// solution#1
 function reverse(str) {
   return str
     .split('')
@@ -14,4 +15,29 @@ function reverse(str) {
     .join('');
 }
 
-module.exports = reverse;
+// solution#2
+function reverse2(str) {
+  let reversed = '';
+  /**
+   * note avoid syntax like this
+   * for(var i=0; i<str.length; i++) {}
+   */
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+// solution#3
+function reverse3(str) {
+  // normal version
+  // return str.split('').reduce((reversed, character) => {
+  //   return character + reversed;
+  // }, '');
+
+  // short version
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+module.exports = { reverse, reverse2, reverse3 };
